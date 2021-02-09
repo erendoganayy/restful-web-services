c9e34b126bf42b475e3fdb76d8af07838d37dd52
eren doganay <erendoganay60@hotmail.com>
2021-02-09 21:27:31 +0300

    internationalized

--- src/main/java/com/restful/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/restful/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.restful.rest.webservices.restfulwebservices.helloworld;
 //Controller
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+@Autowired
+private MessageSource messageSource;
+
+
+
     //GET
     //URI - /hello-word
     //method - "Hello"
@@ -22,5 +31,9 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name));
 
+    }
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language",required = false ) Locale locale){
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 }
